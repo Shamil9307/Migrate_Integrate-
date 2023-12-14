2a82f075e8285743004875bc4d220eb438accc17
shout68 <shout68@yandex.ru>
2023-12-14 16:02:45 +0300

    registation

--- client/src/components/pages/SignupPage.tsx
+++ client/src/components/pages/SignupPage.tsx
@@ -1,35 +1,44 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { SignupFormData } from '../../types/auth';
+import { Container } from 'react-bootstrap';
+import type { SignupFormData } from '../../types/auth';
 import { thunkSignup } from '../../redux/slices/auth/createAsyncThunks';
 import { useAppDispatch } from '../../redux/hooks';
 
 export default function SignupPage(): JSX.Element {
   const dispatch = useAppDispatch();
   return (
-    <Form onSubmit={(e) => {
-      e.preventDefault();
-      const formData = Object.fromEntries(new FormData(e.currentTarget)) as SignupFormData;
-      void dispatch(thunkSignup(formData));
-    }}>
-      <Form.Group className="mb-3" controlId="formBasicName">
-        <Form.Label>Name</Form.Label>
-        <Form.Control type="text" placeholder="Name" name="name"/>
-      </Form.Group>
+    <Container style={{ display: 'flex', justifyContent: 'center', marginTop:'300px' }}>
+      <Form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = Object.fromEntries(new FormData(e.currentTarget)) as SignupFormData;
+          void dispatch(thunkSignup(formData));
+        }}
+      >
+        <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Label>Ваше ФИО</Form.Label>
+          <Form.Control type="text" placeholder="Введите ФИО" name="name" />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" name="email"/>
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Введите email" name="email" />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" name="password"/>
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Пароль</Form.Label>
+          <Form.Control type="password" placeholder="Введите пароль" name="password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>О себе</Form.Label>
+          <Form.Control type="text" placeholder="О себе" name="info" />
+        </Form.Group>
+        <Button variant="primary" type="submit" style={{marginLeft:"35px"}}>
+          Зарегестрироваться
+        </Button>
+      </Form>
+    </Container>
   );
 }
