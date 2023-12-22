ab3a8fa0570e8ae8dcd49ec539508bb98fd35806
shout68 <shout68@yandex.ru>
2023-12-22 14:10:14 +0300

    css

--- client/src/components/ui/UserCard.tsx
+++ client/src/components/ui/UserCard.tsx
@@ -1,32 +1,63 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
 import type { UserType } from '../../types/auth';
-import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { useAppDispatch } from '../../redux/hooks';
 import { thunkApruvedUser, thunkDeniteUser } from '../../redux/slices/user/createAsyncThunks';
 
 type UserProps = {
   user: UserType;
 };
+
 export default function UserCard({ user }: UserProps): JSX.Element {
   const dispatch = useAppDispatch();
-  const selectUser = useAppSelector((store) => store.userSlice.selectedUser);
 
   return (
-    <Card style={{ width: '18rem', margin: '10px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-  <Card.Img variant="top" src={user.img} style={{ height: '300px', width: '100%', objectFit: 'cover' }} />
-  <Card.Body style={{ display: 'flex', flexDirection: 'column', alignItems: 'end', flex: 1 }}>
-    <Card.Title>{user.name}</Card.Title>
-    <Card.Text>{user.info}</Card.Text>
-    <div style={{ marginTop: 'auto', textAlign: 'right' }}>
-      <Button style={{ marginRight: '5px' }} variant="success" onClick={() => void dispatch(thunkApruvedUser(user))}>
-        Подтвердить
-      </Button>
-      <Button variant="danger" onClick={() => void dispatch(thunkDeniteUser(user))}>
-        Отказать
-      </Button>
-    </div>
-  </Card.Body>
-</Card>
-
+    <Card
+      style={{
+        width: '18rem',
+        margin: '10px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        backdropFilter: 'blur(5px)',
+        borderRadius: '15px',
+        backgroundColor: 'rgba(255, 255, 255, 0.1)',
+        border: '1px solid rgba(255, 255, 255, 0.1)',
+      }}
+    >
+      <Card.Img
+        variant="top"
+        src={user.img}
+        style={{ height: '300px', width: '100%', objectFit: 'cover', borderRadius: '15px 15px 0 0' }}
+      />
+      <Card.Body
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          flex: 1,
+          padding: '20px',
+        }}
+      >
+        <Card.Title style={{color:'white'}}>{user.name}</Card.Title>
+        <Card.Text style={{color:'white'}}>{user.info}</Card.Text>
+        <div style={{ marginTop: 'auto', display: 'flex', justifyContent: 'center' }}>
+          <Button
+            style={{ marginRight: '5px', backgroundColor: '#28a745', color: 'white' }}
+            variant="success"
+            onClick={() => void dispatch(thunkApruvedUser(user))}
+          >
+            Подтвердить
+          </Button>
+          <Button
+            style={{ backgroundColor: '#dc3545', color: 'white' }}
+            variant="danger"
+            onClick={() => void dispatch(thunkDeniteUser(user))}
+          >
+            Отказать
+          </Button>
+        </div>
+      </Card.Body>
+    </Card>
   );
 }
