192c169b9613a4bbd470bc68f827158b11287d77
shout68 <shout68@yandex.ru>
2023-12-22 13:47:32 +0300

    fixLegalCard

--- client/src/components/ui/NovostCard.tsx
+++ client/src/components/ui/NovostCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { useAppDispatch } from '../../redux/hooks';
 import { Button, Card } from 'react-bootstrap';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import type { NovostType } from '../../types/novosti';
 import { setSelectedNovost } from '../../redux/slices/novosti';
 import { thunkDeleteNovost } from '../../redux/slices/novosti/createAsyncThunks';
@@ -11,18 +11,23 @@ type NovostCardProps = {
 
 export default function NovostCard({ novost }: NovostCardProps): JSX.Element {
   const dispatch = useAppDispatch();
+  const user = useAppSelector((store) => store.authSlice.user);
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={novost.img} />
       <Card.Body>
         <Card.Title>{novost.title}</Card.Title>
         <Card.Text>{novost.text}</Card.Text>
-        <Button variant="secondary" onClick={() => void dispatch(setSelectedNovost(novost))}>
-          Edit
-        </Button>
-        <Button variant="danger" onClick={() => dispatch(thunkDeleteNovost(novost.id))}>
-          Delete
-        </Button>
+        {user.id === 1 && (
+          <>
+            <Button variant="secondary" onClick={() => void dispatch(setSelectedNovost(novost))}>
+              Edit
+            </Button>
+            <Button variant="danger" onClick={() => dispatch(thunkDeleteNovost(novost.id))}>
+              Delete
+            </Button>
+          </>
+        )}
       </Card.Body>
     </Card>
   );
--- client/src/components/ui/RecCard.tsx
+++ client/src/components/ui/RecCard.tsx
@@ -1,34 +1,59 @@
 import React from 'react';
 import { Button, Card, Col } from 'react-bootstrap';
-import { useAppDispatch } from '../../redux/hooks';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import { thunkDeleteRec } from '../../redux/slices/recpmindation/createAsyncThunks';
 import type { RecomType } from '../../types/recomedation';
 import { setSelectedRes } from '../../redux/slices/recpmindation';
 
 type CardProps = {
   card: RecomType;
-  
 };
 
 export default function RecCard({ card }: CardProps): JSX.Element {
   const dispatch = useAppDispatch();
+  const user = useAppSelector((store) => store.authSlice.user);
   return (
     <Col xs={3} className="p-2 d-flex justify-content-center">
-    <Card style={{ width: '30rem', marginTop: '-60px', marginBottom: '100px' }} className="mb-4 d-flex justify-content-center">
-      <Card.Img variant="top" src={card.img} />
-      <Card.Body className="text-center">
-        <Card.Title>{card.title}</Card.Title>
-        <Card.Text>{card.info}</Card.Text>
-        <div className="d-flex justify-content-center gap-1">
-          <Button variant="danger" style={{ fontFamily: 'Gill Sans, sans-serif', fontSize: '18px', backgroundColor: 'red', width: '150px' }} onClick={() => void dispatch(thunkDeleteRec(card.id))}>
-            Delete
-          </Button>
-          <Button variant="danger" style={{ fontFamily: 'Gill Sans, sans-serif', fontSize: '18px', backgroundColor: '#5fae32', width: '150px' }} onClick={() => dispatch(setSelectedRes(card))}>
-            Edit
-          </Button>
-        </div>
-      </Card.Body>
-    </Card>
-  </Col>
+      <Card
+        style={{ width: '30rem', marginTop: '-60px', marginBottom: '100px' }}
+        className="mb-4 d-flex justify-content-center"
+      >
+        <Card.Img variant="top" src={card.img} />
+        <Card.Body className="text-center">
+          <Card.Title>{card.title}</Card.Title>
+          <Card.Text>{card.info}</Card.Text>
+          <div className="d-flex justify-content-center gap-1">
+            {user.id === 1 && (
+              <>
+                <Button
+                  variant="danger"
+                  style={{
+                    fontFamily: 'Gill Sans, sans-serif',
+                    fontSize: '18px',
+                    backgroundColor: 'red',
+                    width: '150px',
+                  }}
+                  onClick={() => void dispatch(thunkDeleteRec(card.id))}
+                >
+                  Delete
+                </Button>
+                <Button
+                  variant="danger"
+                  style={{
+                    fontFamily: 'Gill Sans, sans-serif',
+                    fontSize: '18px',
+                    backgroundColor: '#5fae32',
+                    width: '150px',
+                  }}
+                  onClick={() => dispatch(setSelectedRes(card))}
+                >
+                  Edit
+                </Button>
+              </>
+            )}
+          </div>
+        </Card.Body>
+      </Card>
+    </Col>
   );
 }
